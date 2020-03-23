8eb7078bea1175e517d80b7569b4ea03bf0063be
Dibyajiban Sahoo <54473532+vestigialcode@users.noreply.github.com>
2020-03-23 20:38:10 +0530

    adding new php script

---
+++ show-youtube-channel.php
@@ -0,0 +1,29 @@
+
+<?php
+/*
+Plugin Name: show-youtube-channel
+Plugin URI: https://github.com/vestigialcode/show-youtube-channel
+Description: Display YouTube sub button and count
+Version: 2020.3.1
+Author: vestigialcode
+Author URI: https://github.com/vestigialcode
+*/
+
+// Exit if accessed directly
+if(!defined('ABSPATH')){
+  exit;
+}
+
+// Load Scripts
+require_once(plugin_dir_path(__FILE__).'/includes/scripts.php');
+
+// Load Class
+require_once(plugin_dir_path(__FILE__).'/includes/class.php');
+
+// Register Widget
+function register_youtubesubs(){
+  register_widget('Youtube_Subs_Widget');
+}
+
+// Hook in function
+add_action('widgets_init', 'register_youtubesubs')
